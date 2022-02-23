948de2001094330197508a01cf91e4fc98df0ba2
JHSeo <qnfqnfqnf@gmail.com>
2022-02-23 21:38:11 +0900

    add auth, logout middleware

--- src/pages/index.server.tsx
+++ src/pages/index.server.tsx
@@ -1,7 +1,30 @@
-export type HomePageProps = {};
+import { AppProps } from 'next/app';
+import { Suspense } from 'react';
 
-function HomePage(props: HomePageProps) {
-  return <div>home page</div>;
+import Note from '@/components/Note.server';
+import Page from '@/components/Page.server';
+import NoteSkeleton from '@/components/NoteSkeleton';
+
+export type HomePageProps = {
+  login?: string;
+  searchText?: string;
+} & AppProps;
+
+function HomePage({ login, searchText = '', router }: HomePageProps) {
+  const { id } = router.query;
+  let selectedId = id ?? null;
+
+  if (Array.isArray(selectedId)) {
+    selectedId = selectedId.join('');
+  }
+
+  return (
+    <Page login={login} searchText={searchText}>
+      <Suspense fallback={<NoteSkeleton isEditing={false} />}>
+        <Note login={login} selectedId={selectedId} isEditing={false} />
+      </Suspense>
+    </Page>
+  );
 }
 
 export default HomePage;
