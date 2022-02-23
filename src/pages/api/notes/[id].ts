5e3fa9de04dd13f0d5b82db7f8ede0bdd9ff2b77
JHSeo <qnfqnfqnf@gmail.com>
2022-02-23 23:16:37 +0900

    update note page

--- src/pages/api/notes/[id].ts
+++ src/pages/api/notes/[id].ts
@@ -5,7 +5,8 @@ import { getUser } from '@/lib/session';
 const notesIdApi = async (req: NextApiRequest, res: NextApiResponse) => {
   const id = (+req.query.id).toString();
 
-  const timeKey = `get item from redis ${req.method}`;
+  const consoleTime = Date.now();
+  const timeKey = `get item from redis:${consoleTime} ${req.method}`;
   console.time(timeKey);
 
   const rawNote = await redis.hget('rsc:notes_2', id);
@@ -20,15 +21,17 @@ const notesIdApi = async (req: NextApiRequest, res: NextApiResponse) => {
   const login = getUser(req);
 
   if (req.method === 'DELETE') {
-    console.time('delete item from redis');
+    const consoleTime = Date.now();
+    console.time(`delete item from redis:${consoleTime}`);
     await redis.hdel('rsc:notes_2', id);
-    console.timeEnd('delete item from redis');
+    console.timeEnd(`delete item from redis:${consoleTime}`);
 
     return res.status(204).send(null);
   }
 
   if (req.method === 'PUT') {
-    console.time('update item from redis');
+    const consoleTime = Date.now();
+    console.time(`update item from redis:${consoleTime}`);
     const updated = {
       id,
       title: (req.body.title ?? '').slice(0, 255),
@@ -37,7 +40,7 @@ const notesIdApi = async (req: NextApiRequest, res: NextApiResponse) => {
       created_by: login,
     };
     await redis.hset('rsc:notes_2', id, JSON.stringify(updated));
-    console.timeEnd('update item from redis');
+    console.timeEnd(`update item from redis:${consoleTime}`);
 
     return res.json(updated);
   }
